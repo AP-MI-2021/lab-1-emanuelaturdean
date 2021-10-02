c87ee79d8d5e1a90e4c9ee555c39bb6836dceab7
emanuelaturdean <turdeanemanuela@gmail.com>
2021-10-02 20:35:23 +0300

    functie produs lista

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ def is_prime(n):
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
-  return 0
-  
-  
+  produs = 1
+  for element in lst:
+    produs = produs * element
+  return produs
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
@@ -29,11 +31,11 @@ def get_cmmdc_v2(x, y):
   # codul vostru aici
   return 0
   
-  
+
 def main():
   # interfata de tip consola aici
-  a = 3
-  print("salut")
+  lista = [3,5,7,1,5]
+  print(get_product(lista))
 
 if __name__ == '__main__':
   main()
