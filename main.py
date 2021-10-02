7de5adc78b4f7be709f5b11bac60924fc34c759d
emanuelaturdean <turdeanemanuela@gmail.com>
2021-10-02 20:25:28 +0300

    laborator1 - test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
+  return 1
   
   
 '''
@@ -10,6 +11,7 @@ Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
+  return 0
   
   
 '''
@@ -17,6 +19,7 @@ Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
+  return 3
   
   
 '''
@@ -24,10 +27,13 @@ Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
+  return 0
   
   
 def main():
   # interfata de tip consola aici
+  a = 3
+  print("salut")
 
 if __name__ == '__main__':
   main()
